55352224ccbef0df76954599a2fb22b89cc386d9
saintshini <diegoruggirello32@gmail.com>
2022-08-12 01:49:49 -0300

    fix: condition response

--- lib/src/write_table.py
+++ lib/src/write_table.py
@@ -5,17 +5,23 @@ table_name = 'visitor_count';
 table = dynamodb.Table(table_name)
 
 def lambda_handler(event, context):
+
+    item={'Item': {'id': 'count', 'visitor_count': '0'}};
     
     response = table.get_item(Key= {'id' : 'count'})
+    print('before',response)
+    if response !== item[0]['Item']['id']:
+        table.put_item(
+            Item={
+                'id':'count',
+                'visitor_count':'0'
+            }
+        )
+        
+    response = table.get_item(Key= {'id' : 'count'})
+    print('after',response)
     count = response['Item']['visitor_count']
     
-    if count:
-        response = table.put_item(
-                Item={
-                    'id':'count',
-                    'visitor_count':'0'
-                }
-         )    
     new_count = str(int(count)+1)
     response = table.update_item(
         Key= {'id': 'count'},
