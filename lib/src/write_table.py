c2d0b168e730af6e5ad1fb4040abf5b57ba18f70
saintshini <diegoruggirello32@gmail.com>
2022-08-13 16:00:26 -0300

    fix: change condition if

--- lib/src/write_table.py
+++ lib/src/write_table.py
@@ -6,29 +6,25 @@ table = dynamodb.Table(table_name)
 
 def lambda_handler(event, context):
 
-    item={'Item': {'id': 'count', 'visitor_count': '0'}};
-    
     response = table.get_item(Key= {'id' : 'count'})
-    print('before',response)
-    if response !== item[0]['Item']['id']:
+    
+    if "Item" not in response:
         table.put_item(
             Item={
                 'id':'count',
                 'visitor_count':'0'
             }
         )
+    else:
+        count = response['Item']['visitor_count']
         
-    response = table.get_item(Key= {'id' : 'count'})
-    print('after',response)
-    count = response['Item']['visitor_count']
-    
-    new_count = str(int(count)+1)
-    response = table.update_item(
-        Key= {'id': 'count'},
-        UpdateExpression= 'set visitor_count = :c',
-        ExpressionAttributeValues= {':c': new_count},
-        ReturnValues= 'UPDATED_NEW'
-    )
-    return {'statusCode': 200,
-        'body': new_count
-    }
+        new_count = str(int(count)+1)
+        response = table.update_item(
+            Key= {'id': 'count'},
+            UpdateExpression= 'set visitor_count = :c',
+            ExpressionAttributeValues= {':c': new_count},
+            ReturnValues= 'UPDATED_NEW'
+        )
+        return {'statusCode': 200,
+            'body': new_count
+        }
