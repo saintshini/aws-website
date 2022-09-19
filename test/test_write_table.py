5abb2ca8f0197e3624c6ed23b8c1a2e628046d10
saintshini <diegoruggirello32@gmail.com>
2022-09-19 00:42:08 -0300

    fix: create unittest

--- test/test_write_table.py
+++ test/test_write_table.py
@@ -1,16 +1,34 @@
 import boto3
 import pytest
-from write_table import write_into_table
+from write_table_01 import write_into_table
 
 @pytest.fixture
-def test_write_into_table():
-    client = boto3.resource('dynamodb')
-    name_table = 'test_visitor_count'
-    table = dynamodb.create_table(TableName=table_name,
-        KeySchema=[{'AttributeName': 'id','KeyType': 'HASH'}],
-        AttributeDefinitions=[{'AttributeName': 'id','AttributeType': 'S'}])
-    data = {'id':'count','visitor_count':'0'}
-    write_into_table(data)
-    response = table.get_item(Key={'id':data['id']})
-    actual_output = response['Item']
-    assert actual_output == data
+def name_table():
+    return 'visitorCounter'
+
+@pytest.fixture
+def create_table_db(dynamodb_client,name_table):
+    dynamodb_client.create_table(TableName=name_table,
+        KeySchema=[
+            {'AttributeName': 'id',
+            'KeyType': 'HASH'
+            }],
+        AttributeDefinitions=[
+            {'AttributeName': 'id',
+            'AttributeType': 'S'
+            }])
+    yield
+
+@pytest.fixture
+def test_write_into_table(create_table_db):
+    write_into_table(create_table_db);
+    data = {'id':'count','visitor_count':'0'};
+    response = create_table_db.get_item(Key={'id':data['id']});
+    actual_output = response['Item'];
+    assert actual_output == data;
+
+@pytest.fixture
+def list_all_tables(dynamodb_client):
+    tables = dynamodb_client.tables.all();
+    assert tables ==['visitorCounter']
+    
